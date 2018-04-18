aa111637c68d897fb993922db74420c917c7a18f
diloob <diloob@hotmail.com>
2018-04-18 14:54:24 +0530

    The source code for inversing the matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This R code is for Programming Assignment 2
+## Cache time consuming computations like inverse of a matrix
 
-## Write a short comment describing this function
+
+## This function returns a list of setter and getter methods.
+## The objects x and m are also siblings of the setter and getter methods
+## x is the matrix (input) and m is the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	m <- NULL
+	set <- function(y) {
+		x <<- y
+		m <<- NULL
+	}
+	get <- function() x
+	setinverse <- function(inv) m <<- inv
+	getinverse <- function() m
+	list(set = set, get = get,
+	     setinverse = setinverse,
+	     getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function finds the inverse of the matrix created in the above
+## makeCacheMatrix function.  The function checks if the inverse is already
+## evaluated.  If not evaluated, it uses the R's solve function to get the
+## inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
